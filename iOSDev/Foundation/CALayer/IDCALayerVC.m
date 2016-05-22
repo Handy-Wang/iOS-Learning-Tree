0e305764b3a904b0171c81122bd69d04746f049a
jiangshan.wjs <jiangshan.wjs@alibaba-inc.com>
2016-05-22 18:04:20 +0800

    [iOS Learning] 修改代码注释 修改deploy target为iOS 7.0

--- iOSDev/Foundation/CALayer/IDCALayerVC.m
+++ iOSDev/Foundation/CALayer/IDCALayerVC.m
@@ -78,8 +78,8 @@ static void runLoopObserverCallBack(CFRunLoopObserverRef observer, CFRunLoopActi
 
 /*
  总结：
-1. 研究CALayer调用UIView的流程，即CALayer的display流程。
-1.1 第一步：setNeedDisplay(UIView) -> setNeedDisplay(CALayer)；
+1. 研究CALayer的display流程。
+1.1 第一步：setNeedDisplay(UIView) -> setNeedDisplay(CALayer)
 1.2 第二步：View Drawing Cycle(Found dirty data) -> display(CALayer) -> displayInContext:(CALayer) -> drawLayer:InContext:(CALayerDelegate<UIView>) -> drawRect:(CALayerDelegate<UIView>)
 1.3 第三步：View Drawing Cycle(Get layer model) -> Compositing Layers(OpenGLES) -> Render in device screen(GPU)
 2. display流程中，有BackingStore的参与，通过符号断点CABackingStoreUpdate_可知。注：只创建CALayer时，需要调用setNeedDisplay才会使drawing cycle发起display流程；而创建UIView时设置frame时就会在UIView的内部调用setNeedDisplay，但是如果不把创建的view添加到父View上的话，CALayer的display方法同样也不会被调用，因为没有父View与说明不需要显示，那么肯定不会有display流程。
