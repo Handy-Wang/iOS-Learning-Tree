dc6cba14828123fc7b008e7df4a87b90d1737f90
jiangshan.wjs <jiangshan.wjs@alibaba-inc.com>
2016-11-23 21:44:14 +0800

    [自动处理键盘遮挡TableView里的textview的功能] 修改目录结构

---
+++ iOSDev/UIKit/EditableView+UncoveredByKeyboard/EditableViewVC.h
@@ -0,0 +1,12 @@
+//
+//  EditableViewVC.h
+//  iOSDev
+//
+//  Created by XiaoShan on 2/26/16.
+//  Copyright © 2016 XiaoShan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditableViewVC : IDBaseTableVC
+@end
