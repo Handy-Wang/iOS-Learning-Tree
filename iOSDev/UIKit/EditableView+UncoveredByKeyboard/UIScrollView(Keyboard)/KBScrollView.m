84037fa8b3ce8c91f1a3fa8f8148a9f40786cfb4
jiangshan.wjs <jiangshan.wjs@alibaba-inc.com>
2016-11-24 21:17:24 +0800

    [自动处理键盘遮挡TableView里的textview的功能] 增加UITextField在UIScrollView里的测试用例

--- iOSDev/UIKit/EditableView+UncoveredByKeyboard/UIScrollView(Keyboard)/KBScrollView.m
+++ iOSDev/UIKit/EditableView+UncoveredByKeyboard/UIScrollView(Keyboard)/KBScrollView.m
@@ -8,8 +8,9 @@
 
 #import "KBScrollView.h"
 #import "AJXKBTextView.h"
+#import "AJXKBTextField.h"
 
-#define kTextViewHeight             (200.0f)
+#define kTextViewHeight             (50.0f)
 #define kTextViewWidth              (400)
 
 @implementation KBScrollView
@@ -25,13 +26,13 @@
         self.backgroundColor = bgColor;
         
         CGRect textView1Frame = CGRectMake(0,
-                                           kScrollViewContentSizeEdge-3*kTextViewHeight,
+                                           2*kTextViewHeight,
                                            kTextViewWidth,
                                            kTextViewHeight);
         AJXKBTextView *textView1 = [[AJXKBTextView alloc] initWithFrame:textView1Frame];
         textView1.layer.borderColor = [UIColor blackColor].CGColor;
         textView1.layer.borderWidth = 1.0f;
-        textView1.text = @"2018世界杯1111111";
+        textView1.text = @"2018世界杯1111111TextView";
         [self addSubview:textView1];
         
         CGRect textView2Frame = CGRectMake(0,
@@ -41,8 +42,30 @@
         AJXKBTextView *textView2 = [[AJXKBTextView alloc] initWithFrame:textView2Frame];
         textView2.layer.borderColor = [UIColor blackColor].CGColor;
         textView2.layer.borderWidth = 1.0f;
-        textView2.text = @"2018世界杯22222222";
+        textView2.text = @"2018世界杯22222222TextView";
         [self addSubview:textView2];
+        
+        CGRect textField1Frame = CGRectMake(0,
+                                            0,
+                                            kTextViewWidth,
+                                            kTextViewHeight);
+        AJXKBTextField *textField1 = [[AJXKBTextField alloc] initWithFrame:textField1Frame];
+        textField1.ts_top = textView1.ts_bottom + 5;
+        textField1.layer.borderColor = [UIColor blackColor].CGColor;
+        textField1.layer.borderWidth = 1.0f;
+        textField1.text = @"2018世界杯333333333TextField";
+        [self addSubview:textField1];
+        
+        CGRect textField2Frame = CGRectMake(0,
+                                            0,
+                                            kTextViewWidth,
+                                            kTextViewHeight);
+        AJXKBTextField *textField2 = [[AJXKBTextField alloc] initWithFrame:textField2Frame];
+        textField2.ts_bottom = textView2.ts_top - 5;
+        textField2.layer.borderColor = [UIColor blackColor].CGColor;
+        textField2.layer.borderWidth = 1.0f;
+        textField2.text = @"2018世界杯44444444TextField";
+        [self addSubview:textField2];
     }
     return self;
 }
